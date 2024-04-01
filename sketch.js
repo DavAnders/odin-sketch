11bdc13f034d1f91a3e48fd7ec983670713b1041
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-01 17:56:01 -0500

    Add resizing and styling

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function handleMouseEnter() {
 }
 
 function handleMouseLeave() {
-  this.style.backgroundColor = "grey";
+  this.style.backgroundColor = "#ff6b6b";
 }
 
 function createGrid(size) {
@@ -25,25 +25,35 @@ function createGrid(size) {
   }
 }
 
-function updateGridWidth(columns) {
-  const columnWidth = 20;
+const gridSizeInput = document.getElementById("gridSizeInput");
+const gridWidthInput = document.getElementById("gridWidthInput");
+const applyGridSizeButton = document.getElementById("changeGrid");
+
+function updateGridWidth(columns, px = 20) {
+  const columnWidth = px;
   const newWidth = columnWidth * columns;
   const gridItemOuters = document.querySelectorAll(".gridItemOuter");
+  const gridItemInners = document.querySelectorAll(".gridItemInner");
   gridItemOuters.forEach(function (item) {
     item.style.width = `${newWidth}px`;
+  });
+  gridItemInners.forEach(function (item) {
+    item.style.width = `${px}px`;
+    item.style.height = `${px}px`;
   });
 }
 document.addEventListener("DOMContentLoaded", createGrid(16));
 document.addEventListener("DOMContentLoaded", updateGridWidth(16));
 
-const gridSizeInput = document.getElementById("gridSizeInput");
-const applyGridSizeButton = document.getElementById("changeGrid");
-
 applyGridSizeButton.addEventListener("click", () => {
-  const gridSize = gridSizeInput.value;
-  const gridSizeNumber = parseInt(gridSize);
-  if (!isNaN(gridSizeNumber) && gridSizeNumber > 0) {
-    createGrid(gridSizeNumber);
-    updateGridWidth(gridSizeNumber);
+  const gridSize = parseInt(gridSizeInput.value);
+  let gridWidth = parseInt(gridWidthInput.value);
+  if (isNaN(gridWidth)) {
+    gridWidth = 20;
+  }
+
+  if (!isNaN(gridSize) && gridSize > 0) {
+    createGrid(gridSize);
+    updateGridWidth(gridSize, gridWidth);
   }
 });
